d0fb784fb28ceace7f490d2bb7788fa3a004199d
rafael <rafaelgebim@gmail.com>
2023-10-10 22:30:22 -0300

    repeticao

---
+++ 3_HANDLEBARS/5_repeticao/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// configurando o handlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard',(req,res)=>{
+    const items = ['item a','item b','item c']
+    
+    res.render('dashboard', {items})
+})
+
+app.get('/', (req, res) => {
+    const usuario = {
+        nome: "Rafael",
+        profissao: "Aluno",
+        idade: 27
+    }
+
+    const auth = true
+
+    res.render('home',{usuario, auth})
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
